5f675ea0f1afd4e09caf659dedc6603a6f58310e
SaifulDA <saifuldaulah24@gmail.com>
2021-09-21 05:47:15 +0700

    tugas pertemuan 2

---
+++ application/controllers/Contoh_1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh_1 extends CI_Controller
+{
+ public function index()
+ {
+ echo "<h1>Perkenalkan</h1>";
+ echo" Nama saya Saiful Daulah <br> ";
+ echo" Saya tinggal di daerah Condet <br> ";
+ echo" Olah raga yang saya sukai adalah Futsal ";
+ }
+}
